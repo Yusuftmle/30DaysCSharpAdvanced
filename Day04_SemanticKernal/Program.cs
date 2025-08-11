05fb5ed965e10bb01d1ba5c59d54ad9231b7c1eb
yusuf temel <yusuf.temeldr@gmail.com>
2025-08-11 16:29:44 +0300

    Add Day 5 gRPC update solution

--- Day04_SemanticKernal/Program.cs
+++ Day04_SemanticKernal/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Day04_SemanticKernal
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+
+namespace Day04_SemanticKernal
 {
     internal class Program
     {
@@ -7,4 +14,4 @@
             Console.WriteLine("Hello, World!");
         }
     }
-}
+} 
---
+++ Day05.gRPC/grpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using Grpc.Core;
+using grpcServer;
+
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5260");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "mistik" });
+
+Console.WriteLine("Greeting: " + reply.Message);
